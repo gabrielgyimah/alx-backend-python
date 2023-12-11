8eeb42cc99e061d8ffb25181495ffda935df1d2a
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-12-11 04:54:19 -0800

    Added the Wait Random module

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """Wait Random Module"""
-
 import asyncio
 import random
-from typing import Union
 
 
-async def wait_random(max_delay: int = 10) -> Union[int, float]:
+async def wait_random(max_delay: int = 10) -> float:
     """Returns a random value between 0 and max delay"""
-    value: Union[int, float] = random.uniform(0, max_delay)
+    wait_time: float = random.uniform(0, max_delay)
     await asyncio.sleep(value)
-    return value
+    return wait_time
