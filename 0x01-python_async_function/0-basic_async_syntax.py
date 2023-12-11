c2d33a174c40338525b7444d366282ab1c7db426
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-12-11 04:49:52 -0800

    Added the AWait Random module

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,10 +3,11 @@
 
 import asyncio
 import random
+from typing import Union
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> Union[int, float]:
     """Returns a random value between 0 and max delay"""
-    value = random.uniform(0, max_delay)
+    value: Union[int, float] = random.uniform(0, max_delay)
     await asyncio.sleep(value)
     return value
