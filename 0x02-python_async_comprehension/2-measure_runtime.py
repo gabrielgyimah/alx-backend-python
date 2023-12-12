0e738d8c368b317faf11509787d198803d3ef13a
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-12-12 04:44:53 -0800

    Added the Measure Runtime module

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Measure Runtime Module """
+import asyncio
+from time import perf_counter
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """ Returns the total runtime for async_comprehension """
+    start = perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return (perf_counter() - start)
