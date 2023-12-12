e896751e89a438c1e4dbd9067599928adafd384b
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-12-12 03:34:02 -0800

    Added the Async Generator Module

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,12 +9,3 @@ async def async_generator():
     for _ in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
-
-
-async def print_yielded_values():
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
-
-asyncio.run(print_yielded_values())
