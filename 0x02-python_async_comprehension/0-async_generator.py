3e4909a527e308f79a8107851b107ec569215dfa
gabrielgyimah <gabrielamanfogyimah@gmail.com>
2023-12-12 04:15:11 -0800

    Annotated te Async Generator module

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, float]:
+async def async_generator() -> Generator[float, None, None]:
     """Generates random numbers every second"""
     for _ in range(10):
         await asyncio.sleep(1)
